5abc3d87e067386c8810dbef38d3928a1a9462e3
Olex7iMatix <86416834+Olex7iMatix@users.noreply.github.com>
2022-11-18 21:00:37 +0100

    I need to learn github

--- include/lexer.h
+++
@@ -1,8 +0,0 @@
-#ifndef CATS_LEXER
-#define CATS_LEXER
-#include <stdio.h>
-#include "printer.h"
-
-char* read_file(const char* path);
-
-#endif
--- include/printer.h
+++
@@ -1,8 +0,0 @@
-#ifndef CATS_PRINTER
-#define CATS_PRINTER
-
-#include <stdio.h>
-
-void print(const char* from, char* msg);
-
-#endif
